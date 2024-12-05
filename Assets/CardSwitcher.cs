995dd44dd91433166443b24ede0e6574087d12e7
uniAnuj <anujshindeofficial@gmail.com>
2024-12-05 17:17:32 +0530

    commit

--- Assets/CardSwitcher.cs
+++ Assets/CardSwitcher.cs
@@ -1,61 +1,134 @@
 using UnityEngine;
 using UnityEngine.UI;
-using DG.Tweening; // Add DOTween namespace
+using UnityEngine.EventSystems;
+using DG.Tweening;
 
-public class CardSwitcher : MonoBehaviour
+public class TrainCardSwitcher : MonoBehaviour
 {
-    public RectTransform card1;
-    public RectTransform card2;
-    public RectTransform card3;
+    public RectTransform[] cards; // List of all cards
+    public Button[] dots;         // Array of dots
+    public RectTransform maskArea; // The visible area for the train
 
-    public Button dot1;
-    public Button dot2;
-    public Button dot3;
+    private float cardWidth; // Width of a single card
+    private float animationDuration = 0.5f; // Duration of the slide animation
+    private int currentCardIndex = 0; // Tracks which card is in the center
+    private float hoverScale = 1.1f; // Scale when hovered
+    private float normalScale = 1f; // Normal scale
+    private float hoverDuration = 0.3f; // Animation duration for scaling
 
-    private Vector2 leftPosition = new Vector2(-500, 0);
-    private Vector2 centerPosition = new Vector2(0, 0);
-    private Vector2 rightPosition = new Vector2(500, 0);
-
-    private float animationDuration = 0.5f; // Duration of the tween
+    public AudioSource audioSource; // Reference to the Audio Source
+    public AudioClip clickSound;    // The click sound effect
 
     void Start()
     {
-        dot1.onClick.AddListener(() => SwitchToCard(1));
-        dot2.onClick.AddListener(() => SwitchToCard(2));
-        dot3.onClick.AddListener(() => SwitchToCard(3));
-    }
+        // Calculate the width of a card based on the mask area
+        cardWidth = maskArea.rect.width;
 
-    void SwitchToCard(int cardNumber)
-    {
-        switch (cardNumber)
+        // Assign button listeners
+        for (int i = 0; i < dots.Length; i++)
         {
-            case 1:
-                card1.DOAnchorPos(centerPosition, animationDuration);
-                card2.DOAnchorPos(rightPosition, animationDuration);
-                card3.DOAnchorPos(leftPosition, animationDuration);
-                HighlightDot(dot1);
-                break;
-            case 2:
-                card1.DOAnchorPos(leftPosition, animationDuration);
-                card2.DOAnchorPos(centerPosition, animationDuration);
-                card3.DOAnchorPos(rightPosition, animationDuration);
-                HighlightDot(dot2);
-                break;
-            case 3:
-                card1.DOAnchorPos(rightPosition, animationDuration);
-                card2.DOAnchorPos(leftPosition, animationDuration);
-                card3.DOAnchorPos(centerPosition, animationDuration);
-                HighlightDot(dot3);
-                break;
+            int index = i; // Local copy for closure
+            dots[i].onClick.AddListener(() => OnDotClick(index));
+        }
+
+        // Align cards at start
+        AlignCards();
+
+        // Highlight the first dot to indicate the first card is active
+        HighlightDot(0);
+
+        // Add hover listeners to cards and dots
+        foreach (var card in cards)
+        {
+            AddHoverEffect(card);
+        }
+        foreach (var dot in dots)
+        {
+            AddHoverEffect(dot.GetComponent<RectTransform>());
         }
     }
 
-    void HighlightDot(Button activeDot)
+    void OnDotClick(int targetIndex)
     {
-        dot1.GetComponent<Image>().color = Color.white;
-        dot2.GetComponent<Image>().color = Color.white;
-        dot3.GetComponent<Image>().color = Color.white;
+        PlayClickSound();
+        SlideToCard(targetIndex);
+    }
 
-        activeDot.GetComponent<Image>().color = Color.blue;
+    void SlideToCard(int targetIndex)
+    {
+        if (targetIndex == currentCardIndex) return; // No need to slide if already selected
+
+        // Calculate the slide distance based on the target index
+        float slideDistance = (targetIndex - currentCardIndex) * -cardWidth;
+
+        // Animate each card to slide by the calculated distance
+        foreach (var card in cards)
+        {
+            card.DOAnchorPosX(card.anchoredPosition.x + slideDistance, animationDuration).SetEase(Ease.InOutCubic);
+        }
+
+        // Update the current index
+        currentCardIndex = targetIndex;
+
+        // Update the highlighted dot
+        HighlightDot(targetIndex);
+    }
+
+    void AlignCards()
+    {
+        for (int i = 0; i < cards.Length; i++)
+        {
+            // Position cards side by side
+            cards[i].anchoredPosition = new Vector2(i * cardWidth, 0);
+        }
+    }
+
+    void HighlightDot(int dotIndex)
+    {
+        foreach (var dot in dots)
+        {
+            dot.GetComponent<Image>().color = Color.white; // Reset all dots to white
+        }
+
+        dots[dotIndex].GetComponent<Image>().color = Color.blue; // Set the active dot to blue
+    }
+
+    void AddHoverEffect(RectTransform target)
+    {
+        EventTrigger trigger = target.gameObject.AddComponent<EventTrigger>();
+
+        // Pointer Enter
+        EventTrigger.Entry entryEnter = new EventTrigger.Entry
+        {
+            eventID = EventTriggerType.PointerEnter
+        };
+        entryEnter.callback.AddListener((eventData) => ScaleUp(target));
+        trigger.triggers.Add(entryEnter);
+
+        // Pointer Exit
+        EventTrigger.Entry entryExit = new EventTrigger.Entry
+        {
+            eventID = EventTriggerType.PointerExit
+        };
+        entryExit.callback.AddListener((eventData) => ScaleDown(target));
+        trigger.triggers.Add(entryExit);
+    }
+
+    void ScaleUp(RectTransform target)
+    {
+        target.DOScale(hoverScale, hoverDuration).SetEase(Ease.OutCubic);
+    }
+
+    void ScaleDown(RectTransform target)
+    {
+        target.DOScale(normalScale, hoverDuration).SetEase(Ease.OutCubic);
+    }
+
+    void PlayClickSound()
+    {
+        if (audioSource != null && clickSound != null)
+        {
+            audioSource.PlayOneShot(clickSound);
+        }
     }
 }
